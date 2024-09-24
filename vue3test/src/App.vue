b333d23f87003b054472e9db7d634e7d54149a57
choetaehyeong <taehyoung0528@gmail.com>
2024-09-24 16:15:43 +0900

    Feat: 기본, 반응형 기초 형태

---
+++ vue3test/src/App.vue
@@ -0,0 +1,44 @@
+<!-- html -->
+<template>
+  <div id="app">
+    <!-- <h1 v-on:click="click_count()">count = {{ count }}</h1> -->
+    <h1 @click="click_count">count = {{ count }}</h1>
+    <div v-if="count > 5">
+      5보다 커요!!!
+    </div>
+    <ul>
+      <li v-for="furit in furits" :key="furit">
+        {{ furit }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<!-- javascript -->
+<script>
+export default({
+  data() {
+    return {
+      count: 0,
+      furits: ['Apple', 'Banana', 'Coconut']
+    }
+  },
+  methods: {
+    click_count(){
+      this.count += 1;
+    }
+  }
+})
+</script>
+
+<!-- css, scss -->
+<style>
+h1{
+  font-size: 50px;
+  color: royalblue;
+}
+ul{
+  li{font-size: 40px;}
+
+}
+</style>
