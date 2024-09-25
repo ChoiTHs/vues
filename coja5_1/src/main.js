df787ba42085d27373ec0560ee2de12307504a71
choetaehyeong <taehyoung0528@gmail.com>
2024-09-26 08:47:50 +0900

    Refactor: coja5_1
    App.vue에서 FirstChild.vue 출력하도록 수정

--- coja5_1/src/main.js
+++ coja5_1/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FirstChild from './components/FirstChild.vue'
 
 createApp(App).mount('#app')
+App.components('Firstchild', FirstChild);
+
+
